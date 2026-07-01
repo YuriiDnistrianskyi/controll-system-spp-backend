354f4794f1ce76c1343d643f6eba8a939b27ad30
YuraDnistriansky <yura.dniester@gmail.com>
2026-07-01 15:55:02 +0300

    Create system_device_repository.py

---
+++ app/repository/orders/system_device_repository.py
@@ -0,0 +1,5 @@
+from app.repository.base_repository import BaseRepository
+from app.database.models.system_device import SystemDevice
+
+class SystemDeviceRepository(BaseRepository[SystemDevice]):
+    pass
