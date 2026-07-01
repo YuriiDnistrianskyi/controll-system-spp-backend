a47d8d007439ba4460d158300b5f9c508a0b2359
YuraDnistriansky <yura.dniester@gmail.com>
2026-07-01 17:36:49 +0300

    Update admin.py

--- app/database/models/admin.py
+++ app/database/models/admin.py
@@ -10,7 +10,3 @@ class Admin(Base):
     last_name: Mapped[String] = mapped_column(String)
     email: Mapped[String] = mapped_column(String)
     password_hash: Mapped[String] = mapped_column(String)
-
-    @staticmethod
-    def create_from_dto(dto: dict) -> object:
-        return Admin(**dto)
