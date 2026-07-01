251361bf29289146c710af6e10def9a716b30c61
YuraDnistriansky <yura.dniester@gmail.com>
2026-07-01 15:54:57 +0300

    Create device_type_repository.py

---
+++ app/repository/orders/device_type_repository.py
@@ -0,0 +1,5 @@
+from app.repository.base_repository import BaseRepository
+from app.database.models.device_type import DeviceType
+
+class DeviceTypeRepository(BaseRepository[DeviceType]):
+    pass
