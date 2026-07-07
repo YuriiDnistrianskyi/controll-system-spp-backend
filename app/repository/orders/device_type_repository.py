073496cdbcb8d0d97e7cef57b5f60b156eeb2516
YuraDnistriansky <yura.dniester@gmail.com>
2026-07-07 16:07:53 +0300

    Update device_type_repository.py

--- app/repository/orders/device_type_repository.py
+++ app/repository/orders/device_type_repository.py
@@ -7,13 +7,13 @@ from app.database.models.device_type import DeviceType
 
 class DeviceTypeRepository:
     async def get_back_battery_type_id(self, session: AsyncSession) -> int:
-        stmt = select(DeviceType).where(DeviceType.type == 'back_battery')
+        stmt = select(DeviceType).where(DeviceType.code == 'BACK_SENSOR')
         result = await session.execute(stmt)
         back_battery_type_id = result.scalar().first()
         return back_battery_type_id
 
     async def get_front_battery_type_id(self, session: AsyncSession) -> int:
-        stmt = select(DeviceType).where(DeviceType.type == 'front_battery')
+        stmt = select(DeviceType).where(DeviceType.code == 'FRONT_SENSOR')
         result = await session.execute(stmt)
         front_battery_type_id = result.scalar().first()
         return front_battery_type_id
