54b39550dc7fb22cd04942867d1aaf28fe057be3
YuraDnistriansky <yura.dniester@gmail.com>
2026-07-10 18:14:56 +0300

    update architecture

--- app/repository/relational/system_repository.py
+++ app/repository/relational/system_repository.py
@@ -1,4 +1,4 @@
-from app.repository.base_repository import BaseRepository
+from app.repository import BaseRepository
 from app.database.models.system import System
 
 class SystemRepository(BaseRepository[System]):
--- app/service/orders/sensor_service.py
+++ app/service/orders/sensor_service.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.repository.orders.device_type_repository import DeviceTypeRepository
+from app.repository.relational import DeviceTypeRepository
 from app.service.base_service import BaseService
-from app.repository.base_repository import BaseRepository
+from app.repository.relational import BaseRepository
 from app.database.models.sensor import Sensor
 from app.schemas.sensor_schemas import CreateSensorSchema, UpdateSensorSchema
 
