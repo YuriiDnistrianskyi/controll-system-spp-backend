470ce76ed2e347c07fbe83b4351bf8c5232afe08
YuraDnistriansky <yura.dniester@gmail.com>
2026-07-14 16:28:01 +0300

    create init_device_handler

--- app/ws/__init__.py
+++ app/ws/__init__.py
@@ -1,11 +1,3 @@
 from app.ws.ws_manager import WsManager
-from app.ws.snapshot_handler import SnapshotHandler
-from app.service import battery_telemetry_service, network_telemetry_service, sensor_telemetry_service
 
 ws_manager = WsManager()
-
-snapshot_handler = SnapshotHandler(
-    battery_service=battery_telemetry_service,
-    network_service=network_telemetry_service,
-    sensor_service=sensor_telemetry_service,
-)
---
+++ app/ws/handlers/init_device_handler.py
@@ -0,0 +1,36 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.service.orders.connected_device_service import ConnectedDeviceService
+from app.service.orders.sensor_service import SensorService
+from app.schemas.connected_device_schemas import CreateConnectedDeviceSchema
+from app.schemas.sensor_schemas import CreateSensorSchema
+
+
+class InitDeviceHandler:
+    def __init__(self, sensor_service: SensorService, connected_device_service: ConnectedDeviceService):
+        self.sensor_service = sensor_service
+        self.connected_device_service = connected_device_service
+
+    async def handle(self, data: dict, session: AsyncSession) -> None:
+        gateway_id = data["gateway_id"]
+        sensors_data = data["sensors"]
+        if sensors_data:
+            for sensor in sensors_data:
+                schema = CreateSensorSchema(
+                    name=sensor["name"],
+                    mac_address=sensor["mac_address"],
+                    type=sensor["type"],
+                    gateway_device_id=gateway_id
+                )
+                await self.sensor_service.create(schema, session)
+
+        connected_devices_data = data["connected_devices"]
+        if connected_devices_data:
+            for device in connected_devices_data: # ?? TODO
+                schema = CreateConnectedDeviceSchema(
+                    name=device["name"],
+                    mac_address=device["mac_address"],
+                    gateway_device_id=gateway_id,
+                    priority=device["priority"]
+                )
+                await self.connected_device_service.create(schema, session)
