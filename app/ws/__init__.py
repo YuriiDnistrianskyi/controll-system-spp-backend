cf1df4177b07fe192de614348b5f9a480d4fe3e7
YuraDnistriansky <yura.dniester@gmail.com>
2026-07-14 17:47:25 +0300

    Update __init__.py

--- app/ws/__init__.py
+++ app/ws/__init__.py
@@ -1,3 +1,10 @@
 from app.ws.ws_manager import WsManager
+from app.ws.dispatcher import Dispatcher
+from app.ws.handlers import snapshot_handler, init_device_handler
+
 
 ws_manager = WsManager()
+dispatcher = Dispatcher(
+    snapshot_handler,
+    init_device_handler
+)
