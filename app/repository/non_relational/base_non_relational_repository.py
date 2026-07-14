b28cf9076d2714634cbdfd666bffe7078fe18be9
YuraDnistriansky <yura.dniester@gmail.com>
2026-07-14 15:11:20 +0300

    update base_non_relation_repository

--- app/repository/non_relational/base_non_relational_repository.py
+++ app/repository/non_relational/base_non_relational_repository.py
@@ -4,18 +4,44 @@ from typing import Any
 from app.core.config import INFLUXDB_BUCKET, INFLUXDB_ORG
 
 class BaseNonRelationalRepository:
-    measurement: str | None = None
+    _measurement: str | None = None
 
     def __init__(self, writer_api, query_api):
-        self.write_api = writer_api
-        self.query_api = query_api
+        self.__write_api = writer_api
+        self.__query_api = query_api
 
-    async def get(self, device_id: int, field: str) -> list[dict[str, Any]]:
+    async def get_records(self, device_id: int, start: str = "-30d") -> list[dict[str, Any]]:
+        query = f"""
+                from(bucket: "{INFLUXDB_BUCKET}")
+                |> range(start: {start})
+                |> filter(fn: (r) =>
+                    r._measurement == "{self._measurement}" and
+                    r.device_id == "{device_id}"
+                )
+                """
+
+        result: list = list()
+
+        #TODO
+        for table in self.__query_api.query(query, org=INFLUXDB_ORG):
+            records = dict()
+            for record in table.records:
+                result.append(
+                    {
+                        'timestamp': "time",
+                        str(record.get_field()): record.get_value()
+                    }
+                )
+            result.append(records)
+
+        return result
+
+    async def get_fields(self, device_id: int, field: str) -> list[dict[str, Any]]:
         query = f"""
                 from(bucket: "{INFLUXDB_BUCKET}")
                 |> range(start: -30d)
                 |> filter(fn: (r) =>
-                    r._measurement == "{self.measurement}" and
+                    r._measurement == "{self._measurement}" and
                     r._field == "{field}" and
                     r.device_id == "{device_id}"
                 )
@@ -23,7 +49,7 @@ class BaseNonRelationalRepository:
 
         result: list = list()
 
-        for table in self.query_api.query(query, org=INFLUXDB_ORG):
+        for table in self.__query_api.query(query, org=INFLUXDB_ORG):
             for record in table.records:
                 result.append(
                     {
@@ -39,7 +65,7 @@ class BaseNonRelationalRepository:
                 from(bucket: "{INFLUXDB_BUCKET}")
                 |> range(start: -30d)
                 |> filter(fn: (r) =>
-                    r._measurement == "{self.measurement}" and
+                    r._measurement == "{self._measurement}" and
                     r.device_id == "{device_id}"
                 )
                 |> last()
@@ -47,7 +73,7 @@ class BaseNonRelationalRepository:
 
         value: dict[str, Any] = dict()
 
-        for table in self.query_api.query(query, org=INFLUXDB_ORG):
+        for table in self.__query_api.query(query, org=INFLUXDB_ORG):
             for record in table.records:
                 value['timestamp'] = record.get_time() #TODO
                 value[record.get_field()] = record.get_value()
@@ -59,7 +85,7 @@ class BaseNonRelationalRepository:
                 from(bucket: "{INFLUXDB_BUCKET}")
                 |> range(start: -30d)
                 |> filter(fn: (r) =>
-                    r._measurement == "{self.measurement}" and
+                    r._measurement == "{self._measurement}" and
                     r._field == "{field}" and
                     r.device_id == "{device_id}"
                 )
@@ -68,7 +94,7 @@ class BaseNonRelationalRepository:
 
         value: dict | None = None
 
-        for table in self.query_api.query(query, org=INFLUXDB_ORG):
+        for table in self.__query_api.query(query, org=INFLUXDB_ORG):
             for record in table.records:
                 value = {
                     'timestamp': record.get_time(),
@@ -78,7 +104,7 @@ class BaseNonRelationalRepository:
         return value
 
     async def add(self, point: Point) -> None:
-        self.write_api.write(
+        self.__write_api.write(
             bucket=INFLUXDB_BUCKET,
             org=INFLUXDB_ORG,
             record=point
--- app/service/telemetry_service.py
+++ app/service/telemetry_service.py
@@ -7,11 +7,11 @@ from app.repository.non_relational.base_non_relational_repository import BaseNon
 
 class TelemetryService:
     def __init__(self, repository: BaseNonRelationalRepository):
-        self.repository = repository
+        self.__repository = repository
+        self.__measurement = repository.measurement
 
-    @staticmethod
-    def create_point(measurement: str, data: dict) -> Point:
-        point = Point(measurement)
+    async def __create_point(self, data: dict) -> Point:
+        point = Point(self.__measurement)
 
         for key, value in data.items():
             if key == 'device_id':
@@ -23,15 +23,19 @@ class TelemetryService:
 
         return point
 
-    async def get(self, device_id: int, field: str) -> list[dict[str, Any]]:
-        return await self.repository.get(device_id, field)
+    async def get_records(self, device_id: int) -> list[dict[str, Any]]:
+        return await self.__repository.get_records(device_id)
+
+    async def get_fields(self, device_id: int, field: str) -> list[dict[str, Any]]:
+        return await self.__repository.get_fields(device_id, field)
 
     async def get_last_record(self, device_id: int) -> dict[str, Any]:
-        return await self.repository.get_last_record(device_id)
+        return await self.__repository.get_last_record(device_id)
 
     async def get_last_field(self, device_id: int, field: str) -> dict[str, Any]:
-        return await self.repository.get_last_field(device_id, field)
+        return await self.__repository.get_last_field(device_id, field)
 
-    async def add(self, point: Point) -> None:
-         await self.repository.add(point)
+    async def add(self, data: dict) -> None:
+        point: Point = await self.__create_point(data)
+        await self.__repository.add(point)
 
