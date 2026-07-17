581227a444428a1e0e5b57ce11436bab810c514a
YuraDnistriansky <yura.dniester@gmail.com>
2026-07-17 17:14:20 +0300

    Create authentication_handler.py

---
+++ app/ws/handlers/authentication_handler.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.service.orders.gateway_device_service import GatewayDeviceService
+from app.schemas.gateway_device_schemas import AuthGatewayDeviceSchema
+from app.ws.handlers.i_handler import IHandler
+
+
+class AuthenticationHandler(IHandler):
+    def __init__(self, gateway_service: GatewayDeviceService):
+        self.gateway_service = gateway_service
+
+    async def handle(self, data: dict, session: AsyncSession) -> int:
+        schema = AuthGatewayDeviceSchema(
+            id=data['gateway_id'],
+            token=data['token']
+        )
+
+        gateway_id: int = await self.gateway_service.authenticate(schema, session)
+        return gateway_id
