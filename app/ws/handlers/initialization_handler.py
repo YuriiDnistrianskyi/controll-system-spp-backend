97ae360e7b2705091242d18b8435bfef829ef37c
YuraDnistriansky <yura.dniester@gmail.com>
2026-07-17 17:14:15 +0300

    Create initialization_handler.py

---
+++ app/ws/handlers/initialization_handler.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.ws.handlers.i_handler import IHandler
+from app.service.orders.gateway_device_service import GatewayDeviceService
+from app.schemas.gateway_device_schemas import InitGatewayDeviceSchema
+
+
+class InitializationHandler(IHandler):
+    def __init__(self, gateway_service: GatewayDeviceService) -> None:
+        self.gateway_service = gateway_service
+
+    async def handle(self, data: dict, session: AsyncSession) -> int:
+        schema = InitGatewayDeviceSchema(
+            token=data['token'],
+            mac_address=data['mac_address']
+        )
+
+        gateway_id: int = await self.gateway_service.initialize(schema, session)
+        return gateway_id
