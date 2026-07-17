d27daa887fe7ffb7a5cd308df4ca4c8091e591db
YuraDnistriansky <yura.dniester@gmail.com>
2026-07-17 17:14:04 +0300

    Update ws_manager.py

--- app/ws/ws_manager.py
+++ app/ws/ws_manager.py
@@ -2,16 +2,25 @@ from fastapi import WebSocket
 
 class WsManager:
     def __init__(self):
-        self.connections: dict[int, WebSocket] = {}
+        self.connections: dict[str, WebSocket] = {}
+        self.auth_connections: dict[int, WebSocket] ={}
 
-    async def connect(self, gateway_device_id: int, ws: WebSocket):
+    async def connect(self, ws: WebSocket, mac_address: str):
         await ws.accept()
-        self.connections[gateway_device_id] = ws
+        self.connections[mac_address] = ws
 
-    async def send(self, gateway_device_id: int, data: dict):
-        ws = self.connections[gateway_device_id]
+    async def switch(self, mac_address: str, gateway_id: int):
+        self.auth_connections[gateway_id] = self.connections[mac_address]
+        self.connections.pop(mac_address)
+
+    async def send(self, gateway_id: int, data: dict):
+        ws = self.auth_connections[gateway_id]
         if ws:
             await ws.send_json(data)
 
-    async def close(self, gateway_device_id: int):
-        self.connections.pop(gateway_device_id)
+    async def close(self, gateway_id: int=None, mac_address: str=None):
+        if gateway_id and gateway_id in self.auth_connections:
+            self.auth_connections.pop(gateway_id)
+
+        if mac_address and mac_address in self.connections:
+            self.connections.pop(mac_address)
