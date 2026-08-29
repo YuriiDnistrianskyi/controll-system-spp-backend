c36bf83786fd1ef08f108fc810561a4b11472cd7
YuraDnistriansky <yura.dniester@gmail.com>
2026-08-29 21:56:38 +0300

    fix: clear git project

--- app/service/orders/battery_data_service.py
+++ app/service/orders/battery_data_service.py
@@ -1,4 +1,3 @@
-from influxdb_client import Point
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.service.base.telemetry_service import TelemetryService
@@ -7,6 +6,9 @@ from app.service.base.telemetry_service import TelemetryService
 class BatteryDataService(TelemetryService):
     async def add(self, data: dict, session: AsyncSession=None) -> None:
         await super().add(data)
+
+        if session is None:
+            return
 
         charge_level = data.get('charge_level')
         if charge_level and charge_level < 20:
